765f2282758383b5824febf06a69d3ee344a5a46
Zaim Arif Bin Md Hamdi <zbin-md-@u90z03s08.42kl.edu.my>
2024-08-20 13:02:59 +0800

    transfer

--- getnextlinepresub/get_next_line_utils.c
+++ getnextlinepresub/get_next_line_utils.c
@@ -6,11 +6,22 @@
 /*   By: zbin-md- <zbin-md-@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 15:55:06 by zbin-md-          #+#    #+#             */
-/*   Updated: 2024/08/14 16:03:13 by zbin-md-         ###   ########.fr       */
+/*   Updated: 2024/08/16 21:00:13 by zbin-md-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strjoin(const char* str1, const char* str2)
+{
+	char	*result;
+	int		total_size;
+	int		i;
+	int		j;
 
+	i = 0;
+	j = 0;
+	total_size = 0;
+
+}
 
 char	*ft_strchr(char *string, int character)
 {
