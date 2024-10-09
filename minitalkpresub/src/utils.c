f0835fe8b2d40cb1b53d6e83f55433be6816088e
Zaim Arif Bin Md Hamdi <zbin-md-@u90z05s02.42kl.edu.my>
2024-10-09 21:05:40 +0800

    utils instead of whole libft, do this for printf too

---
+++ minitalkpresub/src/utils.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zbin-md- <zbin-md-@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 20:59:38 by zbin-md-          #+#    #+#             */
+/*   Updated: 2024/10/09 21:05:06 by zbin-md-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef>
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	num;
+	int	sign;
+
+	i = 0;
+	num = 0;
+	sign = 1;
+	while ((str[i] > 8 && str[i] < 14) || str[i] == ' ')
+	{
+		i++;
+	}
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1 * sign;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = (num * 10) + (str[i] - '0');
+		i++;
+	}
+	return (sign * num);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	int	strcount;
+
+	strcount = 0;
+	while (*str != '\0')
+	{
+		strcount++;
+		str++;
+	}
+	return (strcount);
+}
